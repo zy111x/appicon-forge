8f94b0552ae9e54dc4fd6bcd17ae1f80259fc933
schlibra <schlibra@163.com>
2024-12-18 11:30:24 +0800

    Update the method of exporting presets and optimize the code

--- src/components/settings/import-and-export/index.tsx
+++ src/components/settings/import-and-export/index.tsx
@@ -3,48 +3,16 @@ import { useState } from 'react'
 import { t } from 'i18next'
 import { Import, Share } from 'lucide-react'
 
-import { Button } from '@/components/ui/button.tsx'
+import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
-import { SettingsContainer } from '@/components/ui/styled.ts'
+import { SettingsContainer } from '@/components/ui/styled'
 import { Textarea } from '@/components/ui/textarea'
-import { createColor } from '@/lib/utils.ts'
 import { useStore } from '@/store'
-
-import type { Color } from '@/store/interface'
 
 export const ImportAndExportSettings = () => {
   const cls = 'col-span-2 grid grid-cols-subgrid items-center'
   const [style, setStyle] = useStore((store) => store.styles)
   const [showData, setShowData] = useState('')
-  const keys = [
-    'backgroundColors',
-    'backgroundGradient',
-    'backgroundRotation',
-    'borderColors',
-    'borderGradient',
-    'borderRadius',
-    'borderRotation',
-    'borderWidth',
-    'iconColor',
-    'iconOffset',
-    'iconRotation',
-    'iconShadow',
-    'iconSize',
-    'insetShadows',
-    'padding',
-    'shadows',
-    'textColorRotation',
-    'textColors',
-    'textFont',
-    'textGradient',
-    'textItalic',
-    'textOffset',
-    'textRotation',
-    'textShadow',
-    'textSize',
-    'textValue',
-    'textWeight',
-  ]
   return (
     <SettingsContainer>
       <div className={cls}>
@@ -52,31 +20,7 @@ export const ImportAndExportSettings = () => {
         <Button
           size='icon'
           onClick={() => {
-            const data = []
-            for (let i = 0; i < keys.length; i++) {
-              if (i === 0 || i === 3 || i === 17) {
-                const t: any[] = []
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                style[keys[i]].forEach((item: { value: any }) => {
-                  t.push(item.value)
-                })
-                data[i] = t
-              } else if (i === 11 || i === 13 || i === 15 || i === 23) {
-                const t: any[][] = []
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                style[keys[i]].forEach((item: { value: any }[]) => {
-                  t.push([item[0], item[1], item[2], item[3], item[4].value])
-                })
-                data[i] = t
-              } else {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                data[i] = style[keys[i]]
-              }
-            }
-            setShowData(JSON.stringify(data))
+            setShowData(JSON.stringify(style))
           }}
         >
           <Share />
@@ -87,35 +31,7 @@ export const ImportAndExportSettings = () => {
         <Button
           size='icon'
           onClick={() => {
-            const data = {}
-            for (let i = 0; i < keys.length; i++) {
-              const item = JSON.parse(showData)[i]
-              if (i === 0 || i === 3 || i === 17) {
-                const t: Color[] = []
-                item.forEach((item: string) => {
-                  t.push(createColor(item))
-                })
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                data[keys[i]] = t
-              } else if (i === 11 || i === 13 || i === 15 || i === 23) {
-                const t: Color[][] = []
-                item.forEach((item: any, index: number) => {
-                  t[index] = item
-                  t[index][4] = createColor(item[4])
-                })
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                data[keys[i]] = t
-              } else {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                data[keys[i]] = item
-              }
-            }
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            setStyle(data)
+            setStyle(JSON.parse(showData))
           }}
         >
           <Import />
